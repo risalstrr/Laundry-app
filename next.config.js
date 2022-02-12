d03e913cb887264fab48552c9f6f48e93a19eec2
risalstr <93498117+risalstrr@users.noreply.github.com>
2022-02-12 22:11:10 +0700

    new commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withReactSvg = require("next-react-svg")
+const path = require("path")
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withReactSvg({
+    reactStrictMode: true,
+    include: path.resolve(__dirname, "public"),
+    webpack(config) {
+        return config
+    },
+})
+
+module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
+        screens: {
+            mobile: "420px", // 360px
+            tablet: "810px", //640px
+            // default 768px
+            laptop: "1024px",
+            desktop: "1200px",
+            // default 1200
+
+        },
+        fontFamily: {
+            poppins: ['poppins', 'sans-serif'],
+        },
+
     },
     variants: {
         extend: {},
